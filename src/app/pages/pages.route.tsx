a160f8a990678b4351a501e8abae42ca184f36e4
shiv291 <shiv291@gmail.com>
2020-07-19 19:43:44 +0530

    profile section

--- src/app/pages/pages.route.tsx
+++ src/app/pages/pages.route.tsx
@@ -1,11 +1,17 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
 import React from 'react';
 import DashboardComponent from './Dashboard/DashboardComponent'
+import AgentProfileComponent from "./Profile/AgentProfile";
+import ClientProfileComponent from "./Profile/ClientProfile";
+import FreelancerProfileComponent from "./Profile/FreelancerProfile";
 
 const PageRoute = (props: any) => {    
     return (
         <Switch>
-             <Route exact path={`${props.match.path}/dashboard`} component={DashboardComponent} />            
+             <Route exact path={`${props.match.path}/dashboard`} component={DashboardComponent} />  
+             <Route exact path={`${props.match.path}/agentprofile`} component={AgentProfileComponent} />  
+             <Route exact path={`${props.match.path}/clientprofile`} component={ClientProfileComponent} />  
+             <Route exact path={`${props.match.path}/freelancerprofile`} component={FreelancerProfileComponent} />            
              </Switch>
          
          )
